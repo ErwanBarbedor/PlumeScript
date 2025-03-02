6ed545ca41d12b4a6c7624060b23cf38278b4f2b
Erwan Barbedor <erwan@barbedor.bzh>
2025-03-02 16:41:22 +0100

    Update version number

--- plume-engine/init.lua
+++ plume-engine/init.lua
@@ -1,5 +1,5 @@
 --[[
-Plume🪶 0.20
+Plume🪶 0.21
 Copyright (C) 2024-2025 Erwan Barbedor
 
 Check https://github.com/ErwanBarbedor/Plume
@@ -20,7 +20,7 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 local plume = {}
 
-plume._VERSION = "Plume🪶 0.20"
+plume._VERSION = "Plume🪶 0.21"
 
 -- Load core components using dependency injection pattern
 require "plume-engine/utils"         (plume)
