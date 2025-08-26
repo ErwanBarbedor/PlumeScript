87f236af1672f3a13bedd9309977a5811eac0a6c
Erwan Barbedor <151403651+ErwanBarbedor@users.noreply.github.com>
2025-08-26 14:38:48 +0200

    #61  [BUG] Call inside eval dont work

--- plume-data/engine/parser.lua
+++ plume-data/engine/parser.lua
@@ -154,8 +154,21 @@ return function (plume)
 	            end
 	        end
 
-	        
-	        local terminal = num + idn + quote
+
+	        -- Eval & index
+	        local posarg  = Ct("LIST_ITEM", V"_layer1")
+	        local optnarg = Ct("HASH_ITEM", idn*os*P":"*os*Ct("BODY", V"_layer1"^-1))
+	        local arg = optnarg + posarg
+	        local arglist = Ct("CALL", P"(" * arg^-1 * (os * P"," * os * arg)^0 * P")")
+	        local index = Ct("INDEX", P"[" * V"_layer1" * P"]")
+	    	local directindex = Ct("DIRECT_INDEX", P"." * idn)
+
+	        local evalOpperator = arglist + index + directindex
+
+	    	local access = Ct("EVAL", idn * evalOpperator^1)
+	    	---
+
+	        local terminal = access + num + idn + quote
 	        rules["_layer" .. (#opplist+1)] = os * (terminal + P"(" * V("_layer1") * P")") * os
 
 	        return rules
