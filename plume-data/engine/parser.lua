b531e4271483bf9405364b3a4dab953972643c85
Erwan Barbedor <151403651+ErwanBarbedor@users.noreply.github.com>
2025-10-11 17:27:36 +0200

    #36 Add, in parser, detection of usuals malformed code

--- plume-data/engine/parser.lua
+++ plume-data/engine/parser.lua
@@ -46,10 +46,10 @@ return function (plume)
         end
 
         local function E(errorHandler, pattern)
-        	pattern = pattern or NOT(S"\n")^0
+        	pattern = (pattern or NOT(S"\n")^0) 
             return Cp() * lpeg.C(pattern) * Cp() / function(bpos, content, epos)
                 return {
-                    name = name,
+                    name = "Error",
                     bpos = bpos,
                     epos  = epos-1,
                     content = content,
@@ -186,7 +186,14 @@ return function (plume)
         end
 
         local expr = Ct("EXPR", genALU())
-        local evalBase = Ct("EVAL", (P"(" * expr * P")" + idn) * V"evalOpperator"^0)
+        local evalBase = Ct("EVAL", (
+                  P"("
+                    * (expr + E(plume.error.emptyExprError))
+                * (P")" + E(plume.error.missingClosingBraveError))
+                + idn
+            ) * V"evalOpperator"^0
+        )
+
         local eval = P"$" * evalBase
         local index = Ct("INDEX", P"[" * expr * P"]")
         local directindex = Ct("DIRECT_INDEX", P"." * idn)
@@ -265,7 +272,11 @@ return function (plume)
                     			+ Ct("VARIADIC", P"..." * idn)
                     		) + sugarFlagParam(Ct("FLAG", "?"*idn))
                     		
-        local paramlist  = Ct("PARAMLIST", P"(" * param^-1 * (os * P"," * os * param)^0 * P")")
+        local paramlist  = Ct("PARAMLIST",
+                P"("
+                    * param^-1 * (os * P"," * os * (param + E(plume.error.missingParamError, -param)))^0
+                * P")"
+            )
         local paramlistM = paramlist + E(plume.error.missingParamListError)
         local macro      = Ct("MACRO", P"macro" * (s * idn)^-1 * os * paramlistM * body * _end)
 
