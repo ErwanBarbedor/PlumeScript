c7da35ff6ac85783ce41dd57bcc0a59489444ce4
Erwan Barbedor <151403651+ErwanBarbedor@users.noreply.github.com>
2026-01-03 11:25:57 +0100

    Add license infos

--- plume-data/engine/env.lua
+++ plume-data/engine/env.lua
@@ -1,3 +1,18 @@
+--[[This file is part of Plume
+
+Plume🪶 is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3 of the License.
+
+Plume🪶 is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with Plume🪶.
+If not, see <https://www.gnu.org/licenses/>.
+]]
+
 return function(plume)
 	plume.env = {}
 	plume.env.plume_path = os.getenv("PLUME_PATH")
