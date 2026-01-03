808f3375bd70069ddf20d4066762d7d5a894beac
Erwan Barbedor <151403651+ErwanBarbedor@users.noreply.github.com>
2026-01-03 12:47:58 +0100

    Add license info

--- plume-data/engine/vm/table.lua
+++ plume-data/engine/vm/table.lua
@@ -1,4 +1,19 @@
- TABLE_NEW = function (vm, arg1, arg2)
+--[[This file is part of Plume
+
+Plume🪶 is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3 of the License.
+
+Plume🪶 is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with Plume🪶.
+If not, see <https://www.gnu.org/licenses/>.
+]]
+
+TABLE_NEW = function (vm, arg1, arg2)
     --- Stack 1 table
     --- arg1: -
     --- arg2: -
