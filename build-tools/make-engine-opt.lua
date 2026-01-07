ad800f694fa6b2a9f2f15f5e8000f70bdb4d6349
Erwan Barbedor <151403651+ErwanBarbedor@users.noreply.github.com>
2026-01-07 21:01:50 +0100

    VM-Optimisze: inline require #272

--- build-tools/make-engine-opt.lua
+++ build-tools/make-engine-opt.lua
@@ -1,8 +1,36 @@
 require "build-tools/make-engine"
 
+local plume = require "plume-data/engine/init"
 local lib = require "build-tools/luaParser"
 
+local function apply(ast, f)
+	for i, node in ipairs(ast.children) do
+		ast.children[i] = f(node)
+		if ast.children[i].children then
+			apply(ast.children[i], f)
+		end
+	end
+end
+
+local function inline_require(node)
+	if node.kind == "call" and node.name == "require" then
+		local nodePath = node.children[1].children[1]
+		if nodePath.kind == "string" then
+			local value = nodePath.value:sub(2, -2) -- remove quotes
+			local src = io.open(value..".lua")
+			if src then
+				local ast = lib.parse(src:read("*a"))
+				return ast
+			else
+				print("Cannot inline lib '" .. value .. "'.")
+			end
+		end
+	end
+	return node
+end
+
 local function inline(ast)
+	apply(ast, inline_require)
 end
 
 local base = io.open('plume-data/engine/engine.lua')
