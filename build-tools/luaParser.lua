4157fcc35489057274c166d831000adcd1d4199e
Erwan Barbedor <151403651+ErwanBarbedor@users.noreply.github.com>
2026-01-08 13:13:58 +0100

    VM-Optimizer: inline functions (recursive) #269

--- build-tools/luaParser.lua
+++ build-tools/luaParser.lua
@@ -47,7 +47,7 @@ local patterns = {
     },
     {
         pattern = {
-            "()()([a-zA-Z_][a-zA-Z_%.]*)%(",
+            "()()([a-zA-Z_][a-zA-Z_%.]*)%s*%(",
             "()([a-zA-Z_][a-zA-Z_%.]*)%s*=%s*([a-zA-Z_][a-zA-Z_%.]*)%(",
             "(local)%s+([a-zA-Z_][a-zA-Z_]*)%s*=%s*([a-zA-Z_][a-zA-Z_%.]*)%("
         },
@@ -68,8 +68,8 @@ local patterns = {
     },
     {
         pattern = {
-            "([a-zA-Z_][a-zA-Z_%.]*)%s*('.-')",
-            "([a-zA-Z_][a-zA-Z_%.]*)%s*(\".-\")",
+            "([a-zA-Z_][a-zA-Z_%.0-9]*)%s*('.-')",
+            "([a-zA-Z_][a-zA-Z_%.0-9]*)%s*(\".-\")",
         },
         action = function (state, match)
             local affected = match[3]
@@ -105,7 +105,7 @@ local patterns = {
         end
     },
     {
-        pattern = {"[a-zA-Z_][a-zA-Z_%.]*"},
+        pattern = {"[a-zA-Z_][a-zA-Z_%.0-9]*"},
         action = function (state, match)
             if match[1] == "then" or match[1] == "do" then
                 if state.top.kind == "elseif" then
@@ -261,6 +261,8 @@ local function _export(ast)
             table.insert(result, child.value)
         elseif child.kind == "string" then
             table.insert(result, child.value)
+        elseif child.kind == "blank" then
+            table.insert(result, _export(child))
         elseif not child.kind and child.children then
             table.insert(result, _export(child))
         elseif child.kind == "command" then
