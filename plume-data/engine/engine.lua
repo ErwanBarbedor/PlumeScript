c9b1ed051e829f76b9e2c2f09c8297eb49c2a43f
Erwan Barbedor <151403651+ErwanBarbedor@users.noreply.github.com>
2026-01-15 17:59:42 +0100

    Remove TABLE_INDEX_META

--- plume-data/engine/engine.lua
+++ plume-data/engine/engine.lua
@@ -69,40 +69,39 @@ return function (plume)
 			elseif op == 12 then goto TABLE_INDEX
 			elseif op == 13 then goto TABLE_INDEX_ACC_SELF
 			elseif op == 14 then goto TABLE_SET_META
-			elseif op == 15 then goto TABLE_INDEX_META
-			elseif op == 16 then goto TABLE_SET_ACC
-			elseif op == 17 then goto TABLE_EXPAND
-			elseif op == 18 then goto ENTER_SCOPE
-			elseif op == 19 then goto LEAVE_SCOPE
-			elseif op == 20 then goto BEGIN_ACC
-			elseif op == 21 then goto ACC_TABLE
-			elseif op == 22 then goto ACC_TEXT
-			elseif op == 23 then goto ACC_EMPTY
-			elseif op == 24 then goto ACC_CALL
-			elseif op == 25 then goto ACC_CHECK_TEXT
-			elseif op == 26 then goto JUMP_IF
-			elseif op == 27 then goto JUMP_IF_NOT
-			elseif op == 28 then goto JUMP_IF_NOT_EMPTY
-			elseif op == 29 then goto JUMP
-			elseif op == 30 then goto JUMP_IF_PEEK
-			elseif op == 31 then goto JUMP_IF_NOT_PEEK
-			elseif op == 32 then goto GET_ITER
-			elseif op == 33 then goto FOR_ITER
-			elseif op == 34 then goto OPP_ADD
-			elseif op == 35 then goto OPP_MUL
-			elseif op == 36 then goto OPP_SUB
-			elseif op == 37 then goto OPP_DIV
-			elseif op == 38 then goto OPP_NEG
-			elseif op == 39 then goto OPP_MOD
-			elseif op == 40 then goto OPP_POW
-			elseif op == 41 then goto OPP_LT
-			elseif op == 42 then goto OPP_EQ
-			elseif op == 43 then goto OPP_AND
-			elseif op == 44 then goto OPP_NOT
-			elseif op == 45 then goto OPP_OR
-			elseif op == 46 then goto DUPLICATE
-			elseif op == 47 then goto SWITCH
-			elseif op == 48 then goto END
+			elseif op == 15 then goto TABLE_SET_ACC
+			elseif op == 16 then goto TABLE_EXPAND
+			elseif op == 17 then goto ENTER_SCOPE
+			elseif op == 18 then goto LEAVE_SCOPE
+			elseif op == 19 then goto BEGIN_ACC
+			elseif op == 20 then goto ACC_TABLE
+			elseif op == 21 then goto ACC_TEXT
+			elseif op == 22 then goto ACC_EMPTY
+			elseif op == 23 then goto ACC_CALL
+			elseif op == 24 then goto ACC_CHECK_TEXT
+			elseif op == 25 then goto JUMP_IF
+			elseif op == 26 then goto JUMP_IF_NOT
+			elseif op == 27 then goto JUMP_IF_NOT_EMPTY
+			elseif op == 28 then goto JUMP
+			elseif op == 29 then goto JUMP_IF_PEEK
+			elseif op == 30 then goto JUMP_IF_NOT_PEEK
+			elseif op == 31 then goto GET_ITER
+			elseif op == 32 then goto FOR_ITER
+			elseif op == 33 then goto OPP_ADD
+			elseif op == 34 then goto OPP_MUL
+			elseif op == 35 then goto OPP_SUB
+			elseif op == 36 then goto OPP_DIV
+			elseif op == 37 then goto OPP_NEG
+			elseif op == 38 then goto OPP_MOD
+			elseif op == 39 then goto OPP_POW
+			elseif op == 40 then goto OPP_LT
+			elseif op == 41 then goto OPP_EQ
+			elseif op == 42 then goto OPP_AND
+			elseif op == 43 then goto OPP_NOT
+			elseif op == 44 then goto OPP_OR
+			elseif op == 45 then goto DUPLICATE
+			elseif op == 46 then goto SWITCH
+			elseif op == 47 then goto END
 			end
 
 			::LOAD_CONSTANT::
@@ -146,9 +145,6 @@ return function (plume)
 				goto DISPATCH
 			::TABLE_SET_META::
 				TABLE_SET_META(vm, arg1, arg2)
-				goto DISPATCH
-			::TABLE_INDEX_META::
-				TABLE_INDEX_META(vm, arg1, arg2)
 				goto DISPATCH
 			::TABLE_SET_ACC::
 				TABLE_SET_ACC(vm, arg1, arg2)
