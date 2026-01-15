bb04ef31aceaae6dc879ae13335ed6f505e31e05
Erwan Barbedor <151403651+ErwanBarbedor@users.noreply.github.com>
2026-01-15 07:14:05 +0100

    Add license stuff

--- build-tools/luaBeautifier.lua
+++ build-tools/luaBeautifier.lua
@@ -1,3 +1,19 @@
+--[[This file is part of Plume
+
+Plume🪶 is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3 of the License.
+
+Plume🪶 is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with Plume🪶.
+If not, see <https://www.gnu.org/licenses/>.
+]]
+
+
 package.path = "build-tools/?.lua;build-tools/thenumbernine/?.lua;build-tools/thenumbernine/ext/?.lua;;build-tools/thenumbernine/parser/?.lua;;build-tools/thenumbernine/template/?.lua;" .. package.path
 local Parser = require "parser"
 local tolua = require 'ext.tolua'
