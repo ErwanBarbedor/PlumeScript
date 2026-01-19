0ec2ecc91aa277ebca984de40152157a8ed769d7
Erwan Barbedor <151403651+ErwanBarbedor@users.noreply.github.com>
2026-01-19 18:06:07 +0100

    Default iteration must be an opcode instead of a function #206

--- plume-data/engine/vm/iter.lua
+++ plume-data/engine/vm/iter.lua
@@ -28,18 +28,26 @@ function GET_ITER (vm, arg1, arg2)
         if obj.meta.table.next then
             iter = obj
         else
-            iter = obj.meta.table.iter or vm.plume.defaultMeta.iter
+            iter = obj.meta.table.iter --or vm.plume.defaultMeta.iter
         end
 
-        local value
-        if iter.type == "luaFunction" then
-            value = iter.callable({obj})
-        elseif iter.type == "table" then
-            value = iter
-        elseif iter.type == "macro" then
-            value = _CALL (vm, iter, {obj})
+        local value, state
+        if iter then
+            state = -1
+            if iter.type == "luaFunction" then
+                value = iter.callable({obj})
+            elseif iter.type == "table" then
+                value = iter
+            elseif iter.type == "macro" then
+                value = _CALL (vm, iter, {obj})
+            end
+        else
+            value = obj.table
+            state = 0
         end
+        _STACK_PUSH(vm.mainStack, state)
         _STACK_PUSH(vm.mainStack, value)
+
     else
         _ERROR(vm, vm.plume.error.cannotIterateValue(tobj))
     end 
@@ -51,13 +59,27 @@ end
 --- @param arg2 number Offset of the loop end
 --! inline
 function FOR_ITER (vm, arg1, arg2)
+    local state = _STACK_POP(vm.mainStack)
     local obj = _STACK_POP(vm.mainStack)
-    local iter = obj.meta.table.next
+
     local result
-    if iter.type == "luaFunction" then
-        result = iter.callable()
+    if state>=0 then
+        state = state+1
+
+        if state > #obj then
+            result = vm.empty
+        else
+            result = obj[state]
+        end
+        -- Save state. Offset 1 for local var #2
+        _STACK_SET_FRAMED(vm.variableStack, 1, 0, state)
     else
-        result = _CALL (vm, iter, {obj})
+        local iter = obj.meta.table.next
+        if iter.type == "luaFunction" then
+            result = iter.callable()
+        else
+            result = _CALL (vm, iter, {obj})
+        end
     end
 
     if result == vm.empty then
